71128ef4ec709f8e225c307d59a4d68ecbf68a6f
PROJWEB2 student <njar2008@student.iepscf-uccle.be>
2020-10-26 23:05:14 +0100

    Arthur

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Écrivez votre code JavaScript ici.
+
+//ADD ITEM
+const addItemInput = document.querySelector('.addItemInput');
+const addItemButton = document.querySelector('button.addItemButton');
+
+//list items
+const listItems = document.getElementsByTagName('li');
+
+listDiv.addEventListener('mouseover', (event) => {
+  if(event.target.tagName == 'LI') {
+  event.target.style.textTransform = 'uppercase';
+  }
+});
+
+listDiv.addEventListener('mouseout', (event) => {
+  if(event.target.tagName == 'LI') {
+  event.target.style.textTransform = 'lowercase';
+  }
